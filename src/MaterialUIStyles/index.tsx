bd85f2646b04c8773516b2760f48bd27ff20be96
Shubhro Banerjee <shubhro.banerjee@netwoven.com>
2023-06-15 17:38:40 +0530

    fixes style

--- src/MaterialUIStyles/index.tsx
+++ src/MaterialUIStyles/index.tsx
@@ -5,8 +5,8 @@ import { makeStyles } from '@material-ui/styles';
 const useCommonStyles = makeStyles((theme) => ({
   customContainer: {
     // Apply your custom sizing styles here
-    maxWidth: "100vw", // Example: Set the maximum width to 800px
-
+    maxWidth: "1200px", // Example: Set the maximum width to 800px
+    width:"100vw"
   },
 }));
 
