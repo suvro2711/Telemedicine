1bbedfdb19d37cb423176f9361d470db26e47a7a
Shubhro Banerjee <Srmag0605@gmail.com>
2023-06-19 13:30:31 +0530

    AB#52 fix

--- src/components/common/LogoLink/LogoLink.tsx
+++ src/components/common/LogoLink/LogoLink.tsx
@@ -6,7 +6,7 @@ const LogoLink = (props: Props) => {
   return (
     <a href="https://www.publix.com/">
         <img
-            style={{width:"46px", height:"46px", margin:"1rem"}}
+            style={{width:"46px", height:"46px", margin:"2rem"}}
             alt=""
             src={"/mainlogo.svg"}
         />
