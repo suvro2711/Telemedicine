44ffccb2b5b76a5e3df6b0a10f10b40d4c34b380
Shubhro Banerjee <Srmag0605@gmail.com>
2023-06-21 02:08:07 +0530

    fix doctor's section

--- src/components/DoctorsAppointment/DoctorSection.tsx
+++ src/components/DoctorsAppointment/DoctorSection.tsx
@@ -13,13 +13,46 @@ const DoctorSection: FunctionComponent = () => {
   
   const [diseaseList, setDiseaseList] = useState<Disease[]>(diseaseListIntial)
 
-  const selectDisease = (diseaseName:string) => {
-    const selectDisease:Disease| undefined = diseaseList.find(disease => disease.name === diseaseName)
-    const restOfTheDisease:Disease[] = diseaseList.map(disease => !(disease.name === diseaseName) ? {...disease, isSelected: false} : {name:""})
-    const filteredDisease:Disease[] = restOfTheDisease.filter(disease => disease.name)
-    const newDiseaseList:Disease[] = selectDisease ? [...filteredDisease, {...selectDisease, isSelected:true}] : []
+  const shrikantsSuggsion = (diseaseName:string) => {
+    // const selectDisease:Disease| undefined = diseaseList.find(disease => disease.name === diseaseName)
+    // const restOfTheDisease:Disease[] = diseaseList.map(disease => !(disease.name === diseaseName) ? {...disease, isSelected: false} : {name:""})
+    // const filteredDisease:Disease[] = restOfTheDisease.filter(disease => disease.name)
+    // const newDiseaseList:Disease[] = selectDisease ? [ {...selectDisease, isSelected:true}, ...filteredDisease] : []
+    const newDiseaseList:Disease[] = diseaseList.map(disease => !(disease.name === diseaseName) ? {...disease, isSelected: false} : {...disease, isSelected:true})
     setDiseaseList(newDiseaseList)
     console.log(newDiseaseList)
+  }
+
+  const originalSuggetion = (diseaseName:string) => {
+    const filterDisease = (partialDisease: ({
+      isSelected?: boolean| undefined;
+      name?: string | undefined;
+      iconPath?: string | undefined;
+    })[]):Disease[] =>{
+      const filteredDisease= partialDisease.map((disease)=>{
+        if(disease.name){
+          return({
+            name:disease.name,
+            iconPath:disease.iconPath??"",
+            isSelected:disease.isSelected??false
+          })
+        }
+      }).filter(disease => disease != undefined) as Disease[]
+      return  filteredDisease
+    }
+    const smallWebScreenWidthThreshold = 1896
+    const needToPutSelectedAtFirst = window.innerWidth < smallWebScreenWidthThreshold
+    if(needToPutSelectedAtFirst){
+      const selectDisease = diseaseList.find(disease => disease.name === diseaseName) 
+      const restOfTheDisease:Disease[] = diseaseList.map(disease => !(disease.name === diseaseName) ? {...disease, isSelected: false} : {name:""})
+      const filteredDisease:Disease[] = restOfTheDisease.filter(disease => disease.name)
+      const arrangedDiseaseList = needToPutSelectedAtFirst && selectDisease  ?   [  ...filteredDisease, {...selectDisease, isSelected:true}] :[ {...selectDisease, isSelected:true}, ...filteredDisease]
+      const newDiseaseList:Disease[] = selectDisease ? filterDisease(arrangedDiseaseList): []
+      setDiseaseList(newDiseaseList)
+    }else{
+      const newDiseaseList:Disease[] = diseaseList.map(disease => !(disease.name === diseaseName) ? {...disease, isSelected: false} : {...disease, isSelected:true})
+      setDiseaseList(newDiseaseList)
+    }
   }
 
   return (
@@ -40,10 +73,80 @@ const DoctorSection: FunctionComponent = () => {
             diseaseList.map(disease => <DiseaseCategoryCard  
               key={disease.name}
               disease={disease}
-              selectDisease={selectDisease}
+              selectDisease={originalSuggetion}
             />)
           }
         </div>
+        <div className={styles.filter}>
+          <div className={styles.selectParent}>
+            <div className={styles.select}>
+              <div className={styles.text}>Acute Bronchitis</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>Ear Infection</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>Nausea /Vomiting</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>Phone Consult</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>Stye</div>
+            </div>
+          </div>
+          <div className={styles.selectParent}>
+            <div className={styles.select}>
+              <div className={styles.text}>Allergies</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>Flu (Influenza)</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>Oral Herpes (Cold Sore)</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>Pink Eye</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>Thrush (Oral)</div>
+            </div>
+          </div>
+          <div className={styles.selectParent}>
+            <div className={styles.select}>
+              <div className={styles.text}>COVID-19 Rx (Paxlovid)</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>Migraine Relief</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>Pain Relief</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>Sinus Infection</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>URI-Respiratory Infection</div>
+            </div>
+          </div>
+          <div className={styles.selectParent}>
+            <div className={styles.select}>
+              <div className={styles.text}>Dental Infection</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>Motion Sickness</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>Pediatric</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>Strep Throat</div>
+            </div>
+            <div className={styles.select}>
+              <div className={styles.text}>UTI</div>
+            </div>
+          </div>
+        </div> 
       </div>
     </div>
   )
